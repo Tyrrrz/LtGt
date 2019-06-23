b6ffd509c5b22fdab96158a98e55999e8558835c
Alexey Golub <Tyrrrrrr@gmail.com>
2019-06-23 20:40:49 +0300

    Cleanup HTML grammar

--- LtGt/Internal/HtmlGrammar.cs
+++ LtGt/Internal/HtmlGrammar.cs
@@ -7,70 +7,97 @@ namespace LtGt.Internal
 {
     internal static class HtmlGrammar
     {
+        private static Parser<T> TokenLeft<T>(this Parser<T> parser) => i =>
+        {
+            while (char.IsWhiteSpace(i.Current))
+                i.Advance();
+
+            return parser(i);
+        };
+
+        // Declaration
+
         public static readonly Parser<HtmlDeclaration> HtmlDeclaration =
             from open in Parse.String("<!")
             from name in Parse.LetterOrDigit.AtLeastOnce().Text()
-            from value in Parse.CharExcept('>').Many().Text().Token()
+            from value in Parse.CharExcept('>').Many().Text()
             from close in Parse.Char('>')
             select new HtmlDeclaration(name, value);
 
+        // Comment
+
         public static readonly Parser<HtmlComment> HtmlComment =
             from open in Parse.String("<!--")
-            from content in Parse.AnyChar.Except(Parse.String("-->")).AtLeastOnce().Text().Select(WebUtility.HtmlDecode).Token()
+            from content in Parse.AnyChar.Except(Parse.String("-->")).Many().Text().Select(WebUtility.HtmlDecode).Token()
             from close in Parse.String("-->")
             select new HtmlComment(content);
 
-        private static readonly Parser<HtmlAttribute> HtmlAttributeWithQuotedValue =
-            from name in Parse.LetterOrDigit.Or(Parse.Char('-')).AtLeastOnce().Text()
+        // Attribute
+
+        private static readonly Parser<string> HtmlAttributeName =
+            Parse.CharExcept(c => char.IsControl(c) || char.IsWhiteSpace(c) || c == '\'' || c == '"' || c == '=' || c == '>' || c == '/',
+                    "Invalid character for attribute name")
+                .AtLeastOnce().Text();
+
+        private static readonly Parser<HtmlAttribute> QuotedHtmlAttribute =
+            from name in HtmlAttributeName
             from eq in Parse.Char('=').Token()
             from quoteOpen in Parse.Char('"').Or(Parse.Char('\''))
             from value in Parse.CharExcept(quoteOpen).Many().Text().Select(WebUtility.HtmlDecode)
             from quoteClose in Parse.Char(quoteOpen)
             select new HtmlAttribute(name, value);
 
-        private static readonly Parser<HtmlAttribute> HtmlAttributeWithUnquotedValue =
-            from name in Parse.LetterOrDigit.Or(Parse.Char('-')).AtLeastOnce().Text()
+        private static readonly Parser<HtmlAttribute> UnquotedHtmlAttribute =
+            from name in HtmlAttributeName
             from eq in Parse.Char('=').Token()
             from value in Parse.LetterOrDigit.Many().Text().Select(WebUtility.HtmlDecode)
             select new HtmlAttribute(name, value);
 
-        private static readonly Parser<HtmlAttribute> HtmlAttributeWithoutValue =
-            from name in Parse.LetterOrDigit.Or(Parse.Char('-')).AtLeastOnce().Text()
-            select new HtmlAttribute(name);
+        private static readonly Parser<HtmlAttribute> ValuelessHtmlAttribute = HtmlAttributeName.Select(n => new HtmlAttribute(n));
 
         public static readonly Parser<HtmlAttribute> HtmlAttribute =
-            HtmlAttributeWithQuotedValue.Or(HtmlAttributeWithUnquotedValue).Or(HtmlAttributeWithoutValue);
+            QuotedHtmlAttribute.Or(UnquotedHtmlAttribute).Or(ValuelessHtmlAttribute);
 
-        private static readonly Parser<HtmlElement> HtmlElementSelfClosing =
-            from open in Parse.Char('<')
-            from name in Parse.LetterOrDigit.AtLeastOnce().Text()
-            from attributes in HtmlAttribute.Token().Many()
-            from close in Parse.String("/>").TokenLeft()
-            select new HtmlElement(name, attributes.ToArray());
+        // Element
+
+        private static readonly Parser<string> HtmlElementName = Parse.LetterOrDigit.AtLeastOnce().Text();
 
         private static readonly Parser<HtmlElement> HtmlElementWithChildren =
             from ltOpen in Parse.Char('<')
-            from nameOpen in Parse.LetterOrDigit.AtLeastOnce().Text()
+            from nameOpen in HtmlElementName
             from attributes in HtmlAttribute.Token().Many()
             from gtOpen in Parse.Char('>').TokenLeft()
-            from children in HtmlElementChild.Token().Many()
+            from children in ElementChildHtmlNode.Token().Many()
             from ltClose in Parse.String("</")
             from nameClose in Parse.String(nameOpen)
             from gtClose in Parse.Char('>').TokenLeft()
             select new HtmlElement(nameOpen, attributes.Concat(children).ToArray());
 
-        public static readonly Parser<HtmlElement> HtmlElement = HtmlElementSelfClosing.Or(HtmlElementWithChildren);
+        private static readonly Parser<HtmlElement> SelfClosingHtmlElement =
+            from open in Parse.Char('<')
+            from name in HtmlElementName
+            from attributes in HtmlAttribute.Token().Many()
+            from close in Parse.String("/>").Or(Parse.String(">")).TokenLeft()
+            select new HtmlElement(name, attributes.ToArray<HtmlNode>());
+
+        public static readonly Parser<HtmlElement> HtmlElement = HtmlElementWithChildren.Or(SelfClosingHtmlElement);
+
+        // Text
 
         public static readonly Parser<HtmlText> HtmlText =
             Parse.CharExcept('<').AtLeastOnce().Text().Select(WebUtility.HtmlDecode).Select(t => new HtmlText(t));
 
-        private static readonly Parser<HtmlNode> HtmlElementChild = HtmlElement.Or<HtmlNode>(HtmlText).Or(HtmlComment);
+        // Document
 
         public static readonly Parser<HtmlDocument> HtmlDocument =
             from declarations in HtmlDeclaration.Token().AtLeastOnce()
-            from children in HtmlElementChild.Token().Many()
+            from children in HtmlNode.Token().Many()
             select new HtmlDocument(declarations.Concat(children).ToArray());
 
-        public static readonly Parser<HtmlNode> HtmlNode = HtmlDocument.Or(HtmlElementChild).Or(HtmlDeclaration);
+        // Node
+
+        private static readonly Parser<HtmlNode> ElementChildHtmlNode = HtmlElement.Or<HtmlNode>(HtmlText).Or(HtmlComment);
+
+        public static readonly Parser<HtmlNode> HtmlNode = HtmlDocument.Or(ElementChildHtmlNode).Or(HtmlDeclaration);
     }
 }
