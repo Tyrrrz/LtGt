594818205e52ca365f5ab625dde4ed04f891400a
Alexey Golub <Tyrrrrrr@gmail.com>
2019-06-26 00:02:59 +0300

    Refactor

--- LtGt/HtmlRenderer.cs
+++ LtGt/HtmlRenderer.cs
@@ -11,6 +11,15 @@ namespace LtGt
     /// </summary>
     public partial class HtmlRenderer : IHtmlRenderer
     {
+        private void RenderComment(HtmlComment comment, StringBuilder buffer) =>
+            buffer.Append("<!-- ").Append(comment.Content).Append(" -->");
+
+        private void RenderDeclaration(HtmlDeclaration declaration, StringBuilder buffer) =>
+            buffer.Append("<!").Append(declaration.Name).Append(' ').Append(declaration.Content).Append('>');
+
+        private void RenderText(HtmlText text, StringBuilder buffer) =>
+            buffer.Append(WebUtility.HtmlEncode(text.Content));
+
         private void RenderAttribute(HtmlAttribute attribute, StringBuilder buffer)
         {
             if (attribute.Value != null)
@@ -23,25 +32,14 @@ namespace LtGt
             }
         }
 
-        private void RenderComment(HtmlComment comment, StringBuilder buffer) =>
-            buffer.Append("<!-- ").Append(comment.Content).Append(" -->");
-
-        private void RenderDeclaration(HtmlDeclaration declaration, StringBuilder buffer) =>
-            buffer.Append("<!").Append(declaration.Name).Append(' ').Append(declaration.Content).Append('>');
-
-        private void RenderDocument(HtmlDocument document, StringBuilder buffer)
-        {
-            foreach (var node in document.Children)
-            {
-                RenderNode(node, buffer);
-            }
-        }
-
         private void RenderElement(HtmlElement element, StringBuilder buffer)
         {
+            var attributes = element.GetAttributes().ToArray();
+            var nonAttributes = element.Children.Except(attributes).ToArray();
+
             buffer.Append('<').Append(element.Name);
 
-            foreach (var attribute in element.GetAttributes())
+            foreach (var attribute in attributes)
             {
                 buffer.Append(' ');
                 RenderAttribute(attribute, buffer);
@@ -49,38 +47,44 @@ namespace LtGt
 
             buffer.Append('>');
 
-            foreach (var node in element.Children.Except(element.GetAttributes()))
+            foreach (var node in nonAttributes)
             {
                 RenderNode(node, buffer);
             }
 
             buffer.Append("</").Append(element.Name).Append('>');
         }
-        
-        private void RenderText(HtmlText text, StringBuilder buffer) =>
-            buffer.Append(WebUtility.HtmlEncode(text.Content));
+
+        private void RenderDocument(HtmlDocument document, StringBuilder buffer)
+        {
+            foreach (var node in document.Children)
+            {
+                RenderNode(node, buffer);
+                buffer.AppendLine();
+            }
+        }
 
         private void RenderNode(HtmlNode node, StringBuilder buffer)
         {
             switch (node)
             {
-                case HtmlAttribute attribute:
-                    RenderAttribute(attribute, buffer);
-                    break;
                 case HtmlComment comment:
                     RenderComment(comment, buffer);
                     break;
                 case HtmlDeclaration declaration:
                     RenderDeclaration(declaration, buffer);
                     break;
-                case HtmlDocument document:
-                    RenderDocument(document, buffer);
+                case HtmlText text:
+                    RenderText(text, buffer);
+                    break;
+                case HtmlAttribute attribute:
+                    RenderAttribute(attribute, buffer);
                     break;
                 case HtmlElement element:
                     RenderElement(element, buffer);
                     break;
-                case HtmlText text:
-                    RenderText(text, buffer);
+                case HtmlDocument document:
+                    RenderDocument(document, buffer);
                     break;
             }
         }
